127bdba7a6813cce1ecbb388a9be714fa8526203
Mitsuru Gima <gima0326@yahoo.co.jp>
2020-06-08 22:19:06 +0900

    Update life.clj

--- life.clj
+++ life.clj
@@ -9,38 +9,47 @@
 ;---
 
 (ns examples.life
-   (:import (java.awt Color Dimension)
-	    (javax.swing JPanel JFrame Timer JOptionPane)
-            (java.awt.event ActionListener KeyEvent KeyListener MouseListener)) 
-   (:gen-class))
+　(:import (java.awt Color Dimension)
+　　　　　　(javax.swing JPanel JFrame Timer JOptionPane)
+　　　　　　(java.awt.event ActionListener KeyEvent KeyListener MouseListener)) 
+　(:gen-class))
+
+; ----------------------------------------------------------
+; macro
+; ----------------------------------------------------------
+
+(defmacro alambda
+　[param state & body]
+　　`(letfn [(~'self [~param ~state] ~@body)] ~'self))
+
+(defmacro create-nested-vector
+　[fnc]
+　　`(vec (for [~'h (range 0 HEIGHT)]
+　　　(vec (for [~'w (range 0 WIDTH)]
+　　　　(~fnc [~'h ~'w] HEIGHT WIDTH))))))
 
 ; ----------------------------------------------------------
 ; functional model
 ; ----------------------------------------------------------
 
-(def height 50)
-(def width 50)
-(def point-size 10)
-(def turn-millis 200)
+(def HEIGHT 50)
+(def WIDTH 50)
+(def POINT-SIZE 10)
+(def TURN-MILLIS 200)
 
-(def state-dead 0)
-(def state-born 1)
-(def state-alive 2)
-(def state-add 3)
+(def STATE-DEAD 0)
+(def STATE-BORN 1)
+(def STATE-ALIVE 2)
+(def STATE-ADD 3)
 
-(def neighbours {
-　"1" [-1 -1] "2" [ 0 -1] "3" [ 1 -1]
-　"8" [-1  0]             "4" [ 1  0]
-　"7" [-1  1] "6" [ 0  1] "5" [ 1  1] })
-
-(def template
+(def TEMPLATE
   {
    ;; glider
    "0" #{ [0 1]
        　[1 2]
        　[2 0] [2 1] [2 2] }
 
-   ;;gosper-glider-gun
+   ;; gosper-glider-gun
    "1" #{ [1 5] [1 6]
        　[2 5] [2 6]
        　[11 5] [11 6] [11 7]
@@ -60,64 +69,58 @@
 
 ;----------------------------------------------------------
 
-(defn get-target-state
-　[stat i j]
-　　(nth (nth stat i) j))
+(def NEIGHBOUR-IDXES
+　(create-nested-vector
+　　(fn [trgt-idx h_max w_max]
+　　　(filter
+　　　　(fn [[x y]] (and (< -1 x h_max) (< -1 y w_max) (not (= [x y] trgt-idx))))
+　　　　(map
+　　　　　#(map + trgt-idx %)
+　　　　　(vals { :1 [-1 -1] :2 [ 0 -1] :3 [ 1 -1]
+　　　　　　　　　　:8 [-1 0] :0 [ 0 0] :4 [ 1 0]
+　　　　　　　　　　:7 [-1 1] :6 [ 0 1] :5 [ 1 1] }))))))
 
-(defn get-neighbour-state-idx
-　[i j]
-　　(for [cell (vals neighbours)] (map + (list i j) cell)))
+(defn fn-calc-init-state
+　[stat type]
+　　(fn [trgt-idx & _]
+　　　(if (or (empty? stat) (nil? type))
+　　　　(rand-int 2)
+　　　　(if (contains? (set stat) trgt-idx) STATE-BORN STATE-DEAD))))
 
-(defn get-neighbour-states
-　[stat i j h_max w_max]
-　　(for [[h w] (get-neighbour-state-idx i j)]
-　　　(if (or (> 0 h) (>= h h_max) (> 0 w) (>= w w_max))
-　　　　state-dead
-　　　　(get-target-state stat h w))))
+(defn fn-calc-next-state
+　[stat neighbours]
+　　(fn [trgt-idx & _]
+　　　(let [n-idxes (get-in neighbours trgt-idx)]
+　　　　(case (count (remove zero? (map #(get-in stat %) n-idxes)))
+　　　　　2 (if (= (get-in stat trgt-idx) STATE-DEAD) STATE-DEAD STATE-ALIVE)
+　　　　　3 STATE-BORN
+　　　　　STATE-DEAD))))
 
-(defn get-next-states
-　[stat h_max w_max]
-　　(for [h (range 0 h_max)]
-　　　(for [w (range 0 w_max)]
-　　　　(let [n-states (get-neighbour-states stat h w h_max w_max)]
-　　　　　(case (count (remove zero? n-states))
-　　　　　　2 (if (= (get-target-state stat h w) state-dead) state-dead state-alive)
-　　　　　　3 state-born
-　　　　　　state-dead)))))
+(defn fn-calc-state
+　[stat cell]
+　　(fn [trgt-idx & _]
+　　　(let [cell-stat-current (get-in stat trgt-idx)]
+　　　　(if (= cell trgt-idx)
+　　　　　(if (= cell-stat-current STATE-ADD) STATE-DEAD STATE-ADD)
+　　　　　cell-stat-current))))
 
 ; ----------------------------------------------------------
 ; mutable model
 ; ----------------------------------------------------------
 
-(defn update-states
-　[d]
-　　(dosync (alter d assoc :states
-　　　(get-next-states (:states @d) (:height @d) (:width @d)))))
-
-(defn toggle-pause
-　[d]
-　　(dosync (alter d assoc :pause (not (:pause @d)))))
-
-(defn add-cell
-　[d [i j]]
-　　(let [current-states (:states @d) h_max (:height @d) w_max (:width @d)]
-　　　(dosync (alter d assoc :states
-　　　　(for [h (range 0 h_max)]
-　　　　　(for [w (range 0 w_max)]
-　　　　　　(let [c-state (get-target-state current-states h w)]
-　　　　　　　(if (and (= i h) (= j w))
-　　　　　　　　(if (= c-state state-add) state-dead state-add)
-　　　　　　　　c-state))))))))
+(defn update-stat
+　([cells val] (dosync (alter cells assoc :pause val)))
+　([cells key fnc] (dosync (alter cells assoc key (create-nested-vector fnc)))))
 
 ; ----------------------------------------------------------
 ; gui
 ; ----------------------------------------------------------
 
 (def cell-color {
-　state-dead (Color. 255 255 255)
-　state-born (Color. 210 50 90)
-　state-alive (Color. 110 50 95)
-　state-add (Color. 255 140 0) })
+　STATE-DEAD (Color. 255 255 255)
+　STATE-BORN (Color. 210 50 90)
+　STATE-ALIVE (Color. 110 50 95)
+　STATE-ADD (Color. 255 140 0) })
 
 (defn opt-position
 　[mode h w]
@@ -129,18 +132,16 @@
 　　 "4" (fn [[i j]] (vector j i))))
 
 (defn point-to-screen-rect
-　[size [i j]] 
+　[[i j] size] 
 　　(map #(* size %) [i j 1 1]))
 
 (defn point-to-state-idx
-　[i j size]
-　　(map
-　　　#(- (+ (quot % size) (if (zero? (rem % size)) 0 1)) 1)
-　　　(list j i)))
+　[[i j] size]
+　　(map #(- (+ (quot % size) (if (zero? (rem % size)) 0 1)) 1) [i j]))
 
 (defn fill-point
 　[g pt h w color]
-　　(let [[x y h w] (point-to-screen-rect point-size pt)]
+　　(let [[x y h w] (point-to-screen-rect pt POINT-SIZE)]
 　　　(.setColor g color)
 　　　(.fillRect g y x h w)))
 
@@ -149,28 +150,23 @@
 (defmethod paint :cells
 　[g {:keys [height width states]}]
 　　(doseq [h (range 0 height) w (range 0 width)]
-　　　(let [color (get cell-color (get-target-state states h w))]
-　　　　(fill-point g (list h w) height width color))))
+　　　(let [color (get cell-color (get-in states [h w]))]
+　　　　(fill-point g [h w] height width color))))
 
-; ----------------------------------------------------------
-; macro
-; ----------------------------------------------------------
-
-(defmacro alambda [param state & body]
-　`(letfn [(~'self ~(vec (list param state)) ~@body)]
-　~'self))
-
-(defmacro get-init-states [type stat h_max w_max]
-　`(for [~'h (range 0 ~h_max)]
-　　(for [~'w (range 0 ~w_max)]
-　　　(if (or (nil? ~type) (empty? ~stat)) 
-　　　　(rand-int 2)
-　　　　(if (contains? (set ~stat) (list ~'h ~'w)) state-born　state-dead)))))
+(defn get-dots-max-idx
+　[state]
+　　(inc (last (sort (map (fn [[i j]] (if (> i j) i j)) state)))))
 
 ; ----------------------------------------------------------
 
-(defn get-dots-max-idx [state]
-　(inc (last (sort (map (fn [[i j]] (if (> i j) i j)) state)))))
+(defn create-params
+　[pos idx1 idx2] 
+　　(if (and (not (nil? pos)) (not (= "0" pos)))
+　　　(list pos idx1 idx2)))
+
+(defn create-param-lists [dot dot-idx grid h-idx w-idx]
+　(remove nil? (list (create-params dot dot-idx dot-idx)
+　　　　　　　　　　　　(create-params grid h-idx w-idx))))
 
 (defn create-cells
 　[h_max w_max [dot-type grid-pos dot-pos]]
@@ -179,79 +175,76 @@
 　　　:height h_max
 　　　:width w_max
 　　　:states
-　　　　(let [state (if dot-type
-　　　　　(let [tmp (template (str dot-type))
-　　　　　　param-lst (remove nil? (list
-　　　　　　　;; dot　
-　　　　　　　(if (and (not (nil? dot-pos)) (not (= "0" dot-pos)))
-　　　　　　　　(let [idx (get-dots-max-idx tmp)]
-　　　　　　　　　(list dot-pos idx idx)))
-
-　　　　　　　;; grid
-　　　　　　　(if (and (not (nil? grid-pos)) (not (= "0" grid-pos)))
-　　　　　　　　(list grid-pos h_max w_max))))]
+　　　　(let [ state (if dot-type
+　　　　　(let [ tmp (TEMPLATE (str dot-type))
+　　　　　　　　　dot-idx (get-dots-max-idx tmp)
+　　　　　　　　　param-lst (create-param-lists dot-pos dot-idx grid-pos h_max w_max) ]
 
 　　　　　　;; anaphoric macro
-　　　　　　((alambda params rslt
-　　　　　　　(if (empty? params) rslt
-　　　　　　　　(self (rest params)
-　　　　　　　　　(let [[pos h w] (first params)]
-　　　　　　　　　　(map (opt-position (str pos) h w) rslt)))))
-　　　　　　　param-lst tmp)))]
+　　　　　　((alambda params rslt (if (empty? params) rslt
+ 　　　　　　 (self
+　　　　　 　 　(rest params)
+　　　　　　  　(let [[pos h w] (first params)] (map (opt-position (str pos) h w) rslt)))))
+　　　　   　param-lst tmp))) ]
 
 　　　　　;; init-states
-　　　　　(get-init-states dot-type state h_max w_max)) })
+　　　　　(create-nested-vector (fn-calc-init-state state dot-type))) })
+
+; ----------------------------------------------------------
 
 (defn game-panel
-　[frame cells height width]
+　[frame cells neighbours height width]
 　　(proxy [JPanel ActionListener KeyListener MouseListener] []
 　　　(paintComponent [g]
 　　　　(proxy-super paintComponent g)
 　　　　(paint g @cells))
 
 　　　(actionPerformed [e]
-　　　　(if (not (:pause @cells)) (update-states cells))
+　　　　(if (not (:pause @cells))
+　　　　　(update-stat cells :states (fn-calc-next-state (:states @cells) neighbours)))
 　　　　(.repaint this))
 
 　　　(getPreferredSize []
-　　　　(Dimension. (* (inc width) point-size)
-　　　　(* (inc height) point-size)))
+　　　　(Dimension. (* (inc width) POINT-SIZE)
+　　　　(* (inc height) POINT-SIZE)))
 
 　　　(keyPressed [e]
 　　　　(if (= (.getKeyCode e) KeyEvent/VK_SPACE)
-　　　　　(dosync (toggle-pause cells))))
+　　　　　(update-stat cells (not (:pause @cells)))))
 
 　　　(keyReleased [e])
 　　　(keyTyped [e])
 
 　　　(mouseClicked [e]
-　　　　(let [x (.x (.getPoint e)) y　(.y (.getPoint e))]
-　　　　　(add-cell cells (point-to-state-idx x y point-size)))
-　　　　(.repaint this))
+　　　　(let [ x (.x (.getPoint e)) y (.y (.getPoint e))
+　　　　　　　　clicked-point (point-to-state-idx [y x] POINT-SIZE)]
+　　　　　(update-stat cells :states (fn-calc-state (:states @cells) clicked-point)))
+　　　　　(.repaint this))
 
 　　　(mouseEntered [e])
 　　　(mouseExited [e])
 　　　(mousePressed [e])
 　　　(mouseReleased [e])))
 
-(defn -main [& args]
-　(let [
-   frame (JFrame. "Life")
-　　cells (ref (create-cells height width args))
-　　panel (game-panel frame cells height width)
-　　timer (Timer. turn-millis panel)]
+(defn -main
+　[& args]
+　　(let [
+   　frame (JFrame. "Life")
+　　　cells (ref (create-cells HEIGHT WIDTH args))
+　　　panel (game-panel frame cells NEIGHBOUR-IDXES HEIGHT WIDTH)
+　　　timer (Timer. TURN-MILLIS panel)]
 
-　　(doto panel
-　　　(.setFocusable true)
-　　　(.addMouseListener panel)
-　　　(.addKeyListener panel))
+　　　(doto panel
+　　　　(.setFocusable true)
+　　　　(.addMouseListener panel)
+　　　　(.addKeyListener panel))
 
-　　(doto frame
-　　　(.add panel)
-　　　(.pack)
-　　　(.setVisible true))
+　　　(doto frame
+　　　　(.add panel)
+　　　　(.pack)
+　　　　(.setVisible true))
 
-　　(.start timer)
-　　[cells timer]))
+　　　(.start timer)
+　　　[cells timer]))
 
 ; ----------------------------------------------------------
