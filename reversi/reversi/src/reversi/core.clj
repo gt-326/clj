61b6c6cec011d1d8df36ea1f83a28672c12a17e4
Mitsuru Gima <gt_326@yahoo.co.jp>
2024-07-17 23:01:08 +0900

    以下に着手 　1.1 定数と関数の定義 　1.2 盤面の生成、初期化

---
+++ reversi/reversi/src/reversi/core.clj
@@ -0,0 +1,16 @@
+(ns reversi.core
+  (:use [reversi.board])
+;;  (:require [reversi.board])
+
+;; error
+;;  (:refer [reversi.board])
+  )
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  ;;(println x "Hello, World!")
+
+  (str x " " WALL " " WALL)
+  ;;(str x reversi.board/WALL reversi.board/WALL)
+  )
---
+++ reversi/reversi/test/reversi/core_test.clj
@@ -0,0 +1,11 @@
+(ns reversi.core-test
+  (:require [clojure.test :refer :all]
+            [reversi.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 1 1))))
+
+(deftest foo-test
+  (testing ""
+    (is (= (foo "gima") "gima 3 3"))))
