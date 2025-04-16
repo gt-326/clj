f7d74480b09a370ace24197ed277f81490bae7dc
Mitsuru Gima <gt_326@yahoo.co.jp>
2025-04-16 16:41:41 +0900

    03_Clojureで作るAPI Web サーバーを立ち上げる

--- build-api-with-clojure/src/cljapi/core.clj
+++ build-api-with-clojure/src/cljapi/core.clj
@@ -1,10 +1,20 @@
 ;; ./src/cljapi/core.clj
-(ns cljapi.core)
+(ns cljapi.core
+  (:require
+   [ring.adapter.jetty9 :as jetty]))
 
+(defn ring-handler
+  [_req]
+  {:status 200
+   :body "Hello, Clojure API! \n"})
 
 (defn -main
   [& _args]
-  (println "Hello, Clojure API!"))
+  (jetty/run-jetty ring-handler {:port 8000}))
 
+;; ファイル全体を評価した後で、[1]の式を評価します。
+;; そうすると大量のログがREPLに出力されつつ、Webサーバーが立ち上がります。
 
-;;(range "1")
+(comment
+  (-main) ;; [1]
+  )
