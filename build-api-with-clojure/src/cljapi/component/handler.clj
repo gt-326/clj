80529026a7860f133367cb2f0a1343e4e6d06be2
Mitsuru Gima <gt_326@yahoo.co.jp>
2025-04-28 10:42:13 +0900

    11_Clojureで作るAPI RingMiddlewareを追加してAPIらしくする　その１：APIリクエストをロギングする

--- build-api-with-clojure/src/cljapi/component/handler.clj
+++ build-api-with-clojure/src/cljapi/component/handler.clj
@@ -1,10 +1,15 @@
 (ns cljapi.component.handler
   (:require
-    [com.stuartsierra.component :as component]
-
+    ;; 10_Clojureで作るAPI ルーターを追加する　その３：Systemの再起動を減らす
+    ;; 記載しないと「not found」になってしまう
+    [cljapi.handler.api.greeting]
+    [cljapi.handler.health]
     [cljapi.router :as router]
+    [com.stuartsierra.component :as component]
     [reitit.ring :as ring]
-
+    ;; 11_Clojureで作るAPI RingMiddlewareを追加してAPIらしくする
+    ;; その１：APIリクエストをロギングする
+    [ring.logger :as m.logger]
     ;; 10_Clojureで作るAPI ルーターを追加する　その２：評価のし忘れを防ぐ
     [ring.middleware.lint :as m.lint]
     [ring.middleware.reload :as m.reload]
@@ -23,44 +28,64 @@
 ;;   []
 ;;   (ring/ring-handler router/router))
 
-(defn- build-handler
+(defn- build-handler_
   [prof]
-  (println (str "aaa: " (empty? prof)))
 
   (ring/ring-handler
-   router/router
-   nil
-   {:middleware
-    (if (= prof :prod)
-      []
+    router/router
+    nil
+    {:middleware
+     (if (= prof :prod)
+       []
 
-      ;; 開発時に適用する Ring Middleware です。
-      ;; 今回は wrap-reload だけでなく、
-      ;; ring-devel の提供する他の開発用の Middleware もついでに適用しています。
+       ;; 開発時に適用する Ring Middleware です。
+       ;; 今回は wrap-reload だけでなく、
+       ;; ring-devel の提供する他の開発用の Middleware もついでに適用しています。
 
-      [;; Middleware ①
-       ;; 指定したディレクトリ以下の変更を検知してリロードさせる
-       ;; m.reload/wrap-reload
-       [m.reload/wrap-reload {:dirs ["src"]
-                              ;; オプションはデフォルト
-                              ;; そのままをあえて可視化するために書いています
-                              :reload-compile-errors? true}]
+       [;; Middleware ①
+        ;; 指定したディレクトリ以下の変更を検知してリロードさせる
+        ;; m.reload/wrap-reload
+        [m.reload/wrap-reload {:dirs ["src"]
+                               ;; オプションはデフォルト
+                               ;; そのままをあえて可視化するために書いています
+                               :reload-compile-errors? true}]
 
-       ;; Middleware ②
-       ;; リクエストマップとレスポンスマップが Ring の仕様を満たしているかをチェックする
-       m.lint/wrap-lint
+        ;; Middleware ②
+        ;; リクエストマップとレスポンスマップが Ring の仕様を満たしているかをチェックする
+        m.lint/wrap-lint
 
-       ;; Middleware ③
-       ;; 例外をわかりやすく表示してくれる
-       [m.stacktrace/wrap-stacktrace {:color? true}]
-       ])
-    }
-   ))
+        ;; Middleware ③
+        ;; 例外をわかりやすく表示してくれる
+        [m.stacktrace/wrap-stacktrace {:color? true}]])}))
+
+
+;; 11_Clojureで作るAPI RingMiddlewareを追加してAPIらしくする
+;; その１：APIリクエストをロギングする
+(def ^:private dev-middlewares
+  "開発時だけ有効化する"
+  [[m.reload/wrap-reload {:dirs ["src"]
+                          :reload-compile-errors? true}]
+   m.lint/wrap-lint
+   [m.stacktrace/wrap-stacktrace {:color? true}]])
+
+
+(defn- build-handler
+  [prof]
+  (let [common-middlewares [m.logger/wrap-with-logger]
+        middlewares (if (= prof :prod)
+                      common-middlewares
+                      ;; 開発用のMiddlewareは先に適用する
+                      (apply conj dev-middlewares common-middlewares))]
+    (ring/ring-handler
+      router/router
+      nil
+      {:middleware middlewares})))
 
 
 (defrecord Handler
   ;; 10_Clojureで作るAPI ルーターを追加する　その２：評価のし忘れを防ぐ
-  ;;[handler]
+  ;; [handler]
+
   [handler prof]
 
   component/Lifecycle
@@ -72,6 +97,7 @@
     ;; 10_Clojureで作るAPI ルーターを追加する　その２：評価のし忘れを防ぐ
     ;;  (assoc this :handler (build-handler))
     (assoc this :handler (build-handler prof)))
+
 
   (stop
     [this]
